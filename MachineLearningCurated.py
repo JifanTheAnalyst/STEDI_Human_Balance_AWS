fb5580ec6e88a0f547789333ee7857b225e96a1a
JifanTheAnalyst <93923656+JifanTheAnalyst@users.noreply.github.com>
2024-06-19 17:26:26 +0100

    Update MachineLearningCurated.py

--- MachineLearningCurated.py
+++ MachineLearningCurated.py
@@ -4,7 +4,13 @@ from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue import DynamicFrame
 
+def sparkSqlQuery(glueContext, query, mapping, transformation_ctx) -> DynamicFrame:
+    for alias, frame in mapping.items():
+        frame.toDF().createOrReplaceTempView(alias)
+    result = spark.sql(query)
+    return DynamicFrame.fromDF(result, glueContext, transformation_ctx)
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
 sc = SparkContext()
 glueContext = GlueContext(sc)
@@ -12,24 +18,30 @@ spark = glueContext.spark_session
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-# Script generated for node customer_curated
-customer_curated_node1718367786242 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jiffbucket/cus/"], "recurse": True}, transformation_ctx="customer_curated_node1718367786242")
+# Script generated for node AWS Glue Data Catalog
+AWSGlueDataCatalog_node1718813770596 = glueContext.create_dynamic_frame.from_catalog(database="jiffdb", table_name="accelerometer_trusted", transformation_ctx="AWSGlueDataCatalog_node1718813770596")
 
 # Script generated for node step_trainer
-step_trainer_node1718367787128 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jiffbucket/step_trainer/trusted/"], "recurse": True}, transformation_ctx="step_trainer_node1718367787128")
+step_trainer_node1718367787128 = glueContext.create_dynamic_frame.from_options(format_options={"multiline": False}, connection_type="s3", format="json", connection_options={"paths": ["s3://jiffbucket/step_trainer/landing/"], "recurse": True}, transformation_ctx="step_trainer_node1718367787128")
 
-# Script generated for node Change Schema cus
-ChangeSchemacus_node1718802990724 = ApplyMapping.apply(frame=customer_curated_node1718367786242, mappings=[("serialnumber", "string", "cus_serialnumber", "string"), ("z", "double", "z", "double"), ("birthday", "string", "birthday", "string"), ("sharewithpublicasofdate", "long", "sharewithpublicasofdate", "long"), ("sharewithresearchasofdate", "long", "sharewithresearchasofdate", "long"), ("registrationdate", "long", "registrationdate", "long"), ("customername", "string", "customername", "string"), ("user", "string", "user", "string"), ("sharewithfriendsasofdate", "long", "sharewithfriendsasofdate", "long"), ("y", "double", "y", "double"), ("x", "double", "x", "double"), ("timestamp", "long", "timestamp", "long"), ("email", "string", "email", "string"), ("lastupdatedate", "long", "lastupdatedate", "long"), ("phone", "string", "phone", "string")], transformation_ctx="ChangeSchemacus_node1718802990724")
+# Script generated for node customer_curated
+customer_curated_node1718810986865 = glueContext.create_dynamic_frame.from_catalog(database="jiffdb", table_name="customer_curated", transformation_ctx="customer_curated_node1718810986865")
 
-# Script generated for node Change Schema ste
-ChangeSchemaste_node1718802991789 = ApplyMapping.apply(frame=step_trainer_node1718367787128, mappings=[("sensorreadingtime", "long", "sensorreadingtime", "long"), ("serialnumber", "string", "ste_serialnumber", "string"), ("distancefromobject", "int", "distancefromobject", "int")], transformation_ctx="ChangeSchemaste_node1718802991789")
+# Script generated for node SQL Query
+SqlQuery0 = '''
+select * from step_trainer
+where serialnumber in (select serialnumber from customer_curated
+)
+'''
+SQLQuery_node1718367793073 = sparkSqlQuery(glueContext, query = SqlQuery0, mapping = {"step_trainer":step_trainer_node1718367787128, "customer_curated":customer_curated_node1718810986865}, transformation_ctx = "SQLQuery_node1718367793073")
 
 # Script generated for node Join
-Join_node1718803188381 = Join.apply(frame1=ChangeSchemacus_node1718802990724, frame2=ChangeSchemaste_node1718802991789, keys1=["cus_serialnumber"], keys2=["ste_serialnumber"], transformation_ctx="Join_node1718803188381")
+Join_node1718813782071 = Join.apply(frame1=AWSGlueDataCatalog_node1718813770596, frame2=SQLQuery_node1718367793073, keys1=["timestamp"], keys2=["sensorReadingTime"], transformation_ctx="Join_node1718813782071")
+
 
 # Script generated for node machine_learning_curated
-machine_learning_curated_node1718803245263 = glueContext.getSink(path="s3://jiffbucket/customer/machine_learning/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="machine_learning_curated_node1718803245263")
-machine_learning_curated_node1718803245263.setCatalogInfo(catalogDatabase="jiffdb",catalogTableName="machine_learning_curated")
-machine_learning_curated_node1718803245263.setFormat("json")
-machine_learning_curated_node1718803245263.writeFrame(Join_node1718803188381)
+machine_learning_curated_node1718813864174 = glueContext.getSink(path="s3://jiffbucket/ma/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="machine_learning_curated_node1718813864174")
+machine_learning_curated_node1718813864174.setCatalogInfo(catalogDatabase="jiffdb",catalogTableName="machine_learning_curated_copy")
+machine_learning_curated_node1718813864174.setFormat("json")
+machine_learning_curated_node1718813864174.writeFrame(Join_node1718813782071)
 job.commit()
