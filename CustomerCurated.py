290b25fcf336817c706836c776fc13a4162e3fca
JifanTheAnalyst <93923656+JifanTheAnalyst@users.noreply.github.com>
2024-06-19 15:46:21 +0100

    Update CustomerCurated.py

--- CustomerCurated.py
+++ CustomerCurated.py
@@ -22,6 +22,8 @@ customer_trusted_node1718359910149 = glueContext.create_dynamic_frame.from_optio
 Join_node1718360184152 = Join.apply(frame1=accelerometer_trusted_node1718359913333, frame2=customer_trusted_node1718359910149, keys1=["user"], keys2=["email"], transformation_ctx="Join_node1718360184152")
 
 # Script generated for node customer_curated
-customer_curated_node1718360250335 = glueContext.write_dynamic_frame.from_options(frame=Join_node1718360184152, connection_type="s3", format="json", connection_options={"path": "s3://jiffbucket/customer/curated/", "partitionKeys": []}, transformation_ctx="customer_curated_node1718360250335")
-
+customer_curated_node1718360250335 = glueContext.getSink(path="s3://jiffbucket/customer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="customer_curated_node1718360250335")
+customer_curated_node1718360250335.setCatalogInfo(catalogDatabase="jiffdb",catalogTableName="customer_curated")
+customer_curated_node1718360250335.setFormat("json")
+customer_curated_node1718360250335.writeFrame(Join_node1718360184152)
 job.commit()
